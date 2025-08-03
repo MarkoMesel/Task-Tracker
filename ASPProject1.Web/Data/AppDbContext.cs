61d148f84392f05fbb958e225450de4cb9fa9094
MarkoMesel <mmarkom0001@gmail.com>
2025-08-03 10:54:30 +0200

    Added TodoTask model and AppDbContext Added EF Core migration and database update Added scaffolded TodoTasksController and views

---
+++ ASPProject1.Web/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ASPProject1.Web.Models;
+
+namespace ASPProject1.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TodoTask> Tasks { get; set; }
+    }
+}
