a3138718562def7ed925dab4517e7d7285b8f230
MarkoMesel <mmarkom0001@gmail.com>
2025-08-07 21:54:27 +0200

    Added validation to the TodoTask model (required, max length, display name)

--- TaskTracker.Web/Models/TodoTask.cs
+++ TaskTracker.Web/Models/TodoTask.cs
@@ -1,11 +1,18 @@
-﻿namespace TaskTracker.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Web.Models
 {
     public class TodoTask
     {
         public int Id { get; set; }
+        [Required(ErrorMessage= "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
         public required string Title { get; set; }
+        [StringLength(500, ErrorMessage = "Title cannot be longer than 500 characters")]
         public string? Description { get; set; }
         public bool IsDone { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Due Date")]
         public DateTime? DueDate { get; set; }
     }
 }
