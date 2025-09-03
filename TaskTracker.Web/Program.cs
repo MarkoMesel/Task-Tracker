8f74f7738fe585fedf03ae6764a5734f32dd694c
MarkoMesel <mmarkom0001@gmail.com>
2025-09-03 23:19:19 +0200

    Reverted the Program.cs class to show the updated Home page on startup

--- TaskTracker.Web/Program.cs
+++ TaskTracker.Web/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=TodoTasks}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
